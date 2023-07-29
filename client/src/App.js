86b5510f2b9608421c4a94905542c062ae13ade5
Gaurav Sable <Gaurav-sable>
2023-07-30 01:21:44 +0530

    termswork

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/Header.js';
+import Terms from './components/Terms';
+import Home from './components/Home.js';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/termsandconditions' element={<Terms/>} />
+        </Routes>
       </Router>
     </div>
   );
---
+++ client/src/components/Terms.js
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+
+const Terms = () => {
+
+    return (
+        // <Grid>
+            <div>
+                hellog
+            </div>
+        // </Grid>
+    );
+}
+
+export default Terms;
